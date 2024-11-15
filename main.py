1af4df81c07097ed52949b3d1cce2da8040a5826
AndreyPanakshin <164353054+AndreyPanakshin@users.noreply.github.com>
2024-11-15 20:13:12 +0400

    main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from parser import create_parser
 from imagework import get_img
 from imagework import size_img
 from imagework import plot_histogram
+from imagework import display_histogram
 from imagework import invert_colors
 from imagework import save_image
 from imagework import display_images
@@ -13,7 +14,8 @@ def main():
     try:
         img=get_img(imgdir)
         print(f"size of image: {size_img(img)}")
-        plot_histogram(img)
+        histogram=plot_histogram(img)
+        display_histogram(histogram)
         inverted_image=invert_colors(img)
         save_image(savedir,inverted_image)
         display_images(img,inverted_image)
