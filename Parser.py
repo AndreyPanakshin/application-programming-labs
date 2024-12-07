034d966a0f3913515146df6200d5b2e175bc864e
AndreyPanakshin <164353054+AndreyPanakshin@users.noreply.github.com>
2024-12-07 19:12:09 +0400

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -1,14 +1,14 @@
 import argparse
 
-def create_parser()->tuple[str, str, str, int]:
+def create_parser()->tuple[str, str,int,int]:
     """
     Getting command line arguments
     :return:command line arguments
     """
     parser=argparse.ArgumentParser()
-    parser.add_argument('keyw',type=str)
     parser.add_argument('imagedirectory', type=str)
     parser.add_argument('get_csv', type=str)
-    parser.add_argument('quantity', type=int)
+    parser.add_argument('max_width', type=int)
+    parser.add_argument('max_height', type=int)
     args=parser.parse_args()
-    return args.keyw, args.imagedirectory, args.get_csv,args.quantity
+    return  args.imagedirectory, args.get_csv, args.max_width, args.max_height
