156ad3293fef36b83234b583b39c5d36ae31f088
AndreyPanakshin <164353054+AndreyPanakshin@users.noreply.github.com>
2024-12-07 19:12:52 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
-from Crawler import download_images
 from Parser import create_parser
+from Annotation import annotation
 from Annotation import dataframe
+from Annotation import add_image_dimensions
 from Annotation import filter_images
 from Annotation import sort_area
+from Annotation import statistic
 from Annotation import plot_histogram
 import pandas as pd
 
 def main():
-    keyw, imageditectory, get_csv, quantity=create_parser()
+    imageditectory, get_csv, max_width,max_height=create_parser()
 
     try:
-        download_images(keyw,imageditectory,quantity)
+        annotation(imageditectory, get_csv)
         df = pd.read_csv(get_csv)
-        dataframe(imageditectory,get_csv)
+        dataframe(get_csv)
+        add_image_dimensions(df)
         print(f"dataframe\n{df}")
-        max_width = 1000
-        max_height = 1000
+        statistic(df)
+        print("----")
         df = filter_images(df, max_width, max_height)
         print(f"filter by size dataframe\n{df}")
         df = sort_area(df)
         print(f"sort by area dataframe:\n{df}")
         plot_histogram(df)
-
     except Exception as e:
         print(f"mistake:{e}")
 
 
 if __name__=="__main__":
     main()
-
-
